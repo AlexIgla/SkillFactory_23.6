e266cb6eb031af5ff88cda3182b9db1a9048bd1b
Александр <sanchez.ig.94@mai.ru>
2024-04-02 10:20:57 +0400

    создал бота по образцу

--- TeleBot.py
+++ TeleBot.py
@@ -6,10 +6,6 @@ TOKEN = "7169964806:AAFjazC-ArySYWk5PYyn3YQ8RtZ2HFmsdzc"
 
 bot = telebot.TeleBot(TOKEN)
 
-
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'hello')
 keys = {
     'биткоин': 'BTC',
     'эфириум': 'ETH',
@@ -19,9 +15,17 @@ keys = {
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-    text = ('Чтобы начать работу, введите команду бота в следующем формате:\n<имя валюты> \
+    text = 'Чтобы начать работу, введите команду бота в следующем формате:\n<имя валюты> \
 <в какую валюту перевести> \
-<количество переводимой валюты>')
+<количество переводимой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def help(message: telebot.types.Message):
+    text = ('Доступные валюты:')
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
     bot.reply_to(message, text)
 
 
