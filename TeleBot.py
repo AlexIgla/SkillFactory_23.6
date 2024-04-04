d896a619ea9bd693c82c59b46ce861bb8383e352
Александр <sanchez.ig.94@mai.ru>
2024-04-04 11:46:12 +0400

    исправил код в соответствии с видео 5

--- TeleBot.py
+++ TeleBot.py
@@ -1,6 +1,6 @@
 ﻿import telebot
 from config import keys, TOKEN
-from utils import ConvertionException, CryptoConverter
+from extensions import ConvertionException, CryptoConverter
 
 
 bot = telebot.TeleBot(TOKEN)
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = "7169964806:AAFjazC-ArySYWk5PYyn3YQ8RtZ2HFmsdzc"
 keys = {
-    'биткоин': 'BTC',
-    'эфириум': 'ETH',
+    'евро': 'EUR',
     'доллар': 'USD',
+    'рубль': 'RUB',
 }
